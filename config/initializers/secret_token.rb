c87d09c4e8d7fc5945c031773b17673c50a982af
intinami <nami@intitrading.com>
2015-11-20 12:53:03 +0000

    create new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '5a97e6aed12a588dd72c59ebac2fcdb167fcb8ac9a35b792df933fea1682b967aa53f0c587d8b99970e8c22ef565cc85261a4e8411341cf9371e0039efc02caa'
